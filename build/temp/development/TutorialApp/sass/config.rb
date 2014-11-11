939c8023b5ece9235993c379cecbc60fdeddc74a
Loren Hale <loren.hale@gmail.com>
2014-11-11 16:13:17 +0800

    first commit

---
+++ build/temp/development/TutorialApp/sass/config.rb
@@ -0,0 +1,3 @@
+require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Users/Loren/code/Sencha/TutorialApp/build/.sass-cache'
---
+++ build/temp/development/TutorialApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/Loren/code/Sencha/TutorialApp/app.js
